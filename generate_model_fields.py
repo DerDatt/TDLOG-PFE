1b7dfbdcccd20687c31342b50a7300551d1a89f3
Datt <jakob@adinfinitum.de>
2026-02-08 14:51:15 +0100

    renamed "djangotutorial" to "backend" and adjusted all links: deleted unused "build_pdf.py"

--- generate_model_fields.py
+++ generate_model_fields.py
@@ -1,11 +1,11 @@
 # generate_model_fields.py
 import pandas as pd
-from djangotutorial.appPFE.utils import strip_name_of_underscores_begin_end
+from backend.appPFE.utils import strip_name_of_underscores_begin_end
 # def strip_name_of_underscores_begin_end(name: str) -> str:
 #     # Remove underscores only at the beginning and end, not inside the string
 #     return name.lstrip('_').rstrip('_')
     
-def generate_model_fields_from_csv(csv_path="djangotutorial/appPFE/field_data.csv", output_path="generated_fields.txt"):
+def generate_model_fields_from_csv(csv_path="backend/appPFE/field_data.csv", output_path="generated_fields.txt"):
     """
     Reads CSV and generates Django model field code for copy-pasting.
     """
@@ -30,7 +30,7 @@ def generate_model_fields_from_csv(csv_path="djangotutorial/appPFE/field_data.cs
             
         elif field_type == "ImageField":
             line = f"    {name} = models.CharField(max_length={max_len_texts}, blank=True)"
-            # line = f"    {name} = models.ImageField(upload_to='djangotutorial/media/images')"
+            # line = f"    {name} = models.ImageField(upload_to='images')"
 
         elif field_type == "ChoiceField":
             line = f"    {name} = models.CharField(max_length=100, default='not_chosen')"
@@ -53,6 +53,6 @@ def generate_model_fields_from_csv(csv_path="djangotutorial/appPFE/field_data.cs
 # Execute
 if __name__ == "__main__":
     generate_model_fields_from_csv(
-        csv_path="djangotutorial/appPFE/field_data.csv",
+        csv_path="backend/appPFE/field_data.csv",
         output_path="generated_fields.txt"
     )
